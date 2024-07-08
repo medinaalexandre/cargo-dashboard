11b22cae976e2f8601738ffc7250578498372de8
Medina <contato@medinaalexandre.dev>
2024-07-08 17:12:21 -0300

    chore: update build locally command

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-cd api && cp .env.example .env && docker-compose up --build -d
+#!/usr/bin/bash
+cd api && cp .env.example .env \
+&& docker-compose up --build -d \
+&& docker compose exec php-fpm composer install \
+&& docker compose exec php-fpm php artisan key:generate \
+&& docker compose exec php-fpm php artisan migrate:fresh --seed
